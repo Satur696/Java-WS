b705da469a8e547cbd9088a7cabe7a610a65111f
Satur <82034427+Satur696@users.noreply.github.com>
2023-04-18 22:51:54 -0300

    Add files via upload

---
+++ p1_alg.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+public class p1_alg {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		int y = 32;
+		String nome = "Maria";
+		int idade = 32;
+		double renda = 4000.0;
+		System.out.println(y);
+		double x = 10.4536785;
+		System.out.printf("%.2f\n", x);
+		System.out.printf("%.4f\n", x);
+		System.out.println("Resultado= "+y+"M");
+		System.out.printf("Resultado= %.2f %.4f\n M", x, x);
+		System.out.printf("%s tem %d anos e recebe R$ %.2f reais\n", nome, idade, renda); //%s - string %d - n.inteiro
+	}
+}
